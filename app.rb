007ad1ccb80a933e8e7112cb6534b0c5eb5d1c42
emma-gran-nti-johanneberg <74712733+emma-gran-nti-johanneberg@users.noreply.github.com>
2022-03-01 15:34:10 +0100

    lite prog

--- app.rb
+++ app.rb
@@ -6,3 +6,12 @@ get("/") do
     slim(:index)
 end
 
+get("/fandoms") do
+
+    db = SQLite3::Database.new("db/fandoms.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM fandom")
+    p result
+    slim(:"doors/fandoms",locals:{fandoms:result})
+
+end
