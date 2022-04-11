919ad58b0d53de11638b4269963ad6a040589ceb
emma-gran-nti-johanneberg <74712733+emma-gran-nti-johanneberg@users.noreply.github.com>
2022-04-11 13:41:14 +0200

    Nu är Read och Edit är klara
    Den funkar HURRA!!! :+1:

--- app.rb
+++ app.rb
@@ -11,8 +11,19 @@ get("/fandoms") do
     db = SQLite3::Database.new("db/fandoms.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM fandom")
-    p result
+    #p result
     slim(:"doors/fandoms",locals:{fandom:result})
+
+end
+
+get ('/fandoms/:id/edit') do
+    
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/fandoms.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM fandom WHERE FandomId=?", id).first
+    result2 = db.execute("SELECT * FROM creator WHERE CreatorId=?", id).first
+    slim(:"/doors/edit", locals:{result:result,})
 
 end
 
@@ -21,8 +32,8 @@ get("/fandoms/:id") do
     id = params[:id].to_i
     db = SQLite3::Database.new("db/fandoms.db")
     db.results_as_hash=true
-    result = db.execute("SELECT * FROM fandom WHERE FandomId=?", id).to_s
-    # result2 = db.execute("SELECT * FROM creator WHERE CreatorId=?", id).to_s
+    result = db.execute("SELECT * FROM fandom WHERE FandomId=?", id).first
+    result2 = db.execute("SELECT * FROM creator WHERE CreatorId=?", id).first
     slim(:"doors/show",locals:{result:result,result2:result2})
 
 end
