7c882a461adba0651fb789bbd3c1f91c276c04de
emma-gran-nti-johanneberg <74712733+emma-gran-nti-johanneberg@users.noreply.github.com>
2022-04-11 14:04:08 +0200

    Detta gav fel för update
    Dock på /edit la ditt result2:result2 för att få igenom den. Trodde att jag redan hade skrivit den men hade inte det.

--- app.rb
+++ app.rb
@@ -16,6 +16,17 @@ get("/fandoms") do
 
 end
 
+
+post ('/fandoms/:id/update') do
+    title = params[:title]
+    creator = params[:creator]
+    db = SQLite3::Database.new("db/chinook-crud.db")
+    db.execute("UPDATE fandom SET Name=?", title)
+    db.execute("UPDATE creator SET Name=?", creator)
+    redirect('/fandoms')
+end
+
+
 get ('/fandoms/:id/edit') do
     
     id = params[:id].to_i
@@ -23,7 +34,7 @@ get ('/fandoms/:id/edit') do
     db.results_as_hash = true
     result = db.execute("SELECT * FROM fandom WHERE FandomId=?", id).first
     result2 = db.execute("SELECT * FROM creator WHERE CreatorId=?", id).first
-    slim(:"/doors/edit", locals:{result:result,})
+    slim(:"/doors/edit", locals:{result:result,result2:result2})
 
 end
 
