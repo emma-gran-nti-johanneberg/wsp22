b7b01045c92a3a083b115cb9d6efb4179aa542e9
emma-gran-nti-johanneberg <74712733+emma-gran-nti-johanneberg@users.noreply.github.com>
2022-04-19 13:47:02 +0200

    Bättre version av fandoms/new

--- app.rb
+++ app.rb
@@ -86,14 +86,13 @@ end
 
 post ("/fandoms/new") do
     Name=params[:Name]
-    FandomId=params[:FandomId]
+    Id=params[:Id]
     Author=params[:Author]
-    CreatorId=params[:CreatorId]
     Short_name=params[:Short_name]
-    p "Vi fick in datan #{Name}, #{FandomId}, #{Author}, #{CreatorId} och #{Short_name}."
+    #p "Vi fick in datan #{Name}, #{FandomId}, #{Author}, #{CreatorId} och #{Short_name}."
     db = SQLite3::Database.new("db/fandoms.db")
-    db.execute("INSERT INTO fandom (Name, FandomId, Short_name) VALUES (?,?,?)", Name, FandomId, Short_name)
-    db.execute("INSERT INTO creator (Author, CreatorId) VALUES (?,?)", Author, CreatorId)
+    db.execute("INSERT INTO fandom (Name, FandomId, Short_name) VALUES (?,?,?)", Name, Id, Short_name)
+    db.execute("INSERT INTO creator (Author, CreatorId) VALUES (?,?)", Author, Id)
     redirect("/fandoms")
 end
 
